6011e6fd426e15ac7b148466c68334eb93c49f59
Judge 47 <johnyrocketfingers12@gmail.com>
2018-04-05 14:09:02 +0800

    Added admin program

---
+++ VotingSystem2018_Admin/VotingSystem2018_Admin/VotingSystem2018_Admin/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnVotingResults = New System.Windows.Forms.Button
+        Me.btnBusinessStudentsList = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'btnVotingResults
+        '
+        Me.btnVotingResults.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnVotingResults.Location = New System.Drawing.Point(62, 33)
+        Me.btnVotingResults.Name = "btnVotingResults"
+        Me.btnVotingResults.Size = New System.Drawing.Size(145, 82)
+        Me.btnVotingResults.TabIndex = 0
+        Me.btnVotingResults.Text = "Get Voting Results in Excel"
+        Me.btnVotingResults.UseVisualStyleBackColor = True
+        '
+        'btnBusinessStudentsList
+        '
+        Me.btnBusinessStudentsList.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBusinessStudentsList.Location = New System.Drawing.Point(62, 121)
+        Me.btnBusinessStudentsList.Name = "btnBusinessStudentsList"
+        Me.btnBusinessStudentsList.Size = New System.Drawing.Size(145, 82)
+        Me.btnBusinessStudentsList.TabIndex = 1
+        Me.btnBusinessStudentsList.Text = "Get list of Business Students"
+        Me.btnBusinessStudentsList.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(264, 261)
+        Me.Controls.Add(Me.btnBusinessStudentsList)
+        Me.Controls.Add(Me.btnVotingResults)
+        Me.Name = "Form1"
+        Me.Text = "Voting_System Admin"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnVotingResults As System.Windows.Forms.Button
+    Friend WithEvents btnBusinessStudentsList As System.Windows.Forms.Button
+
+End Class
---
+++ VotingSystem2018_Admin/VotingSystem2018_Admin/VotingSystem2018_Admin/Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.Office.Core
+Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class Form1
+
+    Dim con As SqlConnection = New SqlConnection("server=localhost;Initial Catalog=db1;Integrated Security=True")
+    Dim cmd As SqlCommand
+    Dim rdr As SqlDataReader
+
+    Dim myExcelApp As Excel.Application
+    Dim myExcelWorkbooks As Excel.Workbooks
+    Dim myExcelWorkbook As Excel.Workbook
+    Dim misValue As Object = System.Reflection.Missing.Value
+
+    Dim fileName As [String] = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\Report.xls"
+
+
+    Private Sub btnVotingResults_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnVotingResults.Click
+
+    End Sub
+
+    Private Sub btnBusinessStudentsList_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBusinessStudentsList.Click
+
+    End Sub
+
+    Sub export_excel(ByVal sql)
+        con.Open()
+        cmd = New SqlCommand(sql, con)
+        rdr = cmd.ExecuteReader
+        If rdr.HasRows Then
+            myExcelApp = New Excel.ApplicationClass()
+            myExcelApp.Visible = True
+            myExcelWorkbooks = myExcelApp.Workbooks
+
+
+            myExcelWorkbook = myExcelWorkbooks.Open(fileName, misValue, misValue, misValue, misValue, misValue, _
+             misValue, misValue, misValue, misValue, misValue, misValue, _
+             misValue, misValue, misValue)
+
+            Dim myExcelWorksheet As Excel.Worksheet = DirectCast(myExcelWorkbook.ActiveSheet, Excel.Worksheet)
+
+            myExcelWorksheet.Range("A1", misValue).Formula = "From"
+            myExcelWorksheet.Range("B1", misValue).Formula = txt1.Text
+            myExcelWorksheet.Range("D1", misValue).Formula = "To"
+            myExcelWorksheet.Range("E1", misValue).Formula = txt2.Text
+
+            myExcelWorksheet.Range("A3", misValue).Formula = "ID"
+            myExcelWorksheet.Range("B3", misValue).Formula = "Product"
+            myExcelWorksheet.Range("C3", misValue).Formula = "Price"
+
+            Dim ctr As Integer = 4
+            Do While rdr.Read()
+
+                myExcelWorksheet.Cells(ctr, 1) = rdr("ID")
+                myExcelWorksheet.Cells(ctr, 2) = rdr("product")
+                myExcelWorksheet.Cells(ctr, 3) = rdr("price")
+                ctr = ctr + 1
+            Loop
+        Else
+            MsgBox("No Record Found", MsgBoxStyle.Critical)
+        End If
+        con.Close()
+    End Sub
+End Class
